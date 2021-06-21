f62d6c5ce433e49c031e6cceca38a242d2cc7a74
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-06-21 17:53:24 +0100

    Add createMock function and loop test both good and bad

--- internal/execute/execute_test.go
+++ internal/execute/execute_test.go
@@ -1,13 +1,15 @@
 package execute
 
 import (
+	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/go-git/go-git/v5"
 	"github.com/google/go-github/v35/github"
-	"github.com/ministryofjustice/cloud-platform-git-xargs/internal/git"
+	local "github.com/ministryofjustice/cloud-platform-git-xargs/internal/git"
 )
 
 // mockRepo uses the a relatively small repository to test against.
@@ -25,18 +27,26 @@ func mockRepo() (r *github.Repository) {
 	return
 }
 
+// createMock simply executes the mockRepo function call making it much
+// easier to read.
+func createMock() (repoDir string, tree *git.Worktree) {
+	repo := mockRepo()
+	client := github.NewClient(nil)
+
+	repoDir, localRepo, _ := local.Clone(repo, client)
+
+	tree, _ = localRepo.Worktree()
+
+	return
+}
+
 // TestCommand mocks a repository and clones it locally. It then performs a series of steps
 // that determine if the function Command is working as expected.
 func TestCommand(t *testing.T) {
 	// Setup test by creating mock repo and cloning locally.
 	t.Parallel()
 
-	repo := mockRepo()
-	client := github.NewClient(nil)
-
-	repoDir, localRepo, _ := git.Clone(repo, client)
-
-	tree, _ := localRepo.Worktree()
+	repoDir, tree := createMock()
 
 	// Should fail if a command with len == 0 is used in argument.
 	err := Command(repoDir, "", tree, false)
@@ -47,7 +57,7 @@ func TestCommand(t *testing.T) {
 	// Will pass if a file called file.md exists in each directory.
 	err = Command(repoDir, "touch file.md", tree, true)
 	if err != nil {
-		t.Error("Unable to run command. Fail.")
+		t.Error("Unable to execute command when loop == true")
 	}
 
 	_ = filepath.Walk(repoDir, func(path string, info fs.FileInfo, err error) error {
@@ -63,5 +73,19 @@ func TestCommand(t *testing.T) {
 		return nil
 	})
 	// test: if loop == false it creates only one file
+	repoDir, tree = createMock()
+	filePath := repoDir + "/cmd/file.md"
+
+	err = Command(repoDir, "touch file.md", tree, true)
+	if err != nil {
+		t.Error("Unable to run command when loop == false")
+	}
+
+	fmt.Println(filePath)
+	_, err = os.Stat(filePath)
+	if os.IsExist(err) {
+		t.Error("File exists where it shouldn't; want no file.md, got file.md")
+	}
+
 	// test: if I pass a bad command, it fails.
 }
