1d13be0775e94995d2aa2666e68e8de984f374fc
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-05-09 14:07:05 +0100

    test(fetchRepositories): mock the github client using go-github-mock
    This should always have been mocked - we don't want to have an internet connection to run unit tests. This would also fall foul of API rate limiting if left unauthenticated. This way we can mock the response from the GitHub API and just test this function works correctly.

--- internal/get/get_test.go
+++ internal/get/get_test.go
@@ -1,29 +1,92 @@
 package get
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/google/go-github/v35/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
 )
 
-// TestGetRepository tests the FetchRepositories function by passing a static
-// GitHub client, an organisation and a pattern of a repository name. It should
-// pass if a single repository is returned and matches the value of blob.
+// TestGetRepository tests the getRepository function by mocking the github API
+// and responding with a mocked repository collection.
 func TestFetchRepositories(t *testing.T) {
-	client := github.NewClient(nil)
-	org := "ministryofjustice"
-	blob := "cloud-platform-cli"
+	mockedClient := mock.NewMockedHTTPClient(
+		mock.WithRequestMatchPages(
+			mock.GetOrgsReposByOrg,
+			[]github.Repository{
+				{
+					Name: github.String("repo-A-on-first-page"),
+				},
+				{
+					Name: github.String("repo-B-on-first-page"),
+				},
+			},
+			[]github.Repository{
+				{
+					Name: github.String("repo-C-on-second-page"),
+				},
+				{
+					Name: github.String("repo-D-on-second-page"),
+				},
+			},
+		),
+	)
 
-	repos, err := FetchRepositories(client, org, blob)
-	if err != nil {
-		t.Error("Unable to fetch repositories:", err)
+	type args struct {
+		client *github.Client
+		org    string
+		blob   string
 	}
-
-	for _, repo := range repos {
-		fmt.Println(repo.Name)
-		if repo.Name != &blob {
-			t.Errorf("Repository name not found; got %p, wanted cloud-platform-cli", repo.Name)
-		}
+	tests := []struct {
+		name    string
+		args    args
+		want    []*github.Repository
+		wantErr bool
+	}{
+		{
+			name: "get correct repositories",
+			args: args{
+				client: github.NewClient(mockedClient),
+				org:    "test",
+				blob:   "repo",
+			},
+			want: []*github.Repository{
+				{
+					Name: github.String("repo-A-on-first-page"),
+				},
+				{
+					Name: github.String("repo-B-on-first-page"),
+				},
+				{
+					Name: github.String("repo-C-on-second-page"),
+				},
+				{
+					Name: github.String("repo-D-on-second-page"),
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "pass incorrect blob",
+			args: args{
+				client: github.NewClient(mockedClient),
+				org:    "test",
+				blob:   "obviouslyWrong",
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FetchRepositories(tt.args.client, tt.args.org, tt.args.blob)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FetchRepositories() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FetchRepositories() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
